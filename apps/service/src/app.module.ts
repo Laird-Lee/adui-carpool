78a4e74a2b3d5771662c249b49c63f84895393f5
LiBo <laird.lee@outlook.com>
2023-09-07 10:44:14 +0800

    feat：config

--- apps/service/src/app.module.ts
+++ apps/service/src/app.module.ts
@@ -3,9 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SentryInterceptor, SentryModule } from '@ntegral/nestjs-sentry';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
+import authConfig from './config/auth.config';
+import databaseConfig from './config/database.config';
+import fileConfig from './config/file.config';
+import mailConfig from './config/mail.config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, authConfig, databaseConfig, fileConfig, mailConfig],
+      envFilePath: ['.env'],
+    }),
     SentryModule.forRoot({
       dsn: 'https://893262f51e867f452e6ab86cf2c884ab@o985795.ingest.sentry.io/4505831510179840',
       debug: true,
---
+++ apps/service/src/config/common/config.enum.ts
@@ -0,0 +1,10 @@
+export enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+}
+
+export enum FileDriver {
+  LOCAL = 'local',
+  S3 = 's3',
+}
